b0089cac5eade61ccdbdbfd6c14699694bcb1302
syenalc <shunpei.yopuedo@gmail.com>
2024-09-02 00:09:05 +0900

    コメント追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   // CORSを有効にする
+//   app.enableCors();
+  
+//   await app.listen(4000);
+// }
+// bootstrap();
+
+
+//上記はlambda環境でのサーバーの起動を想定
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'http';
+import * as awsServerlessExpress from 'aws-serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    await app.init();
+    cachedServer = awsServerlessExpress.createServer(app.getHttpAdapter().getInstance());
+  }
+  return cachedServer;
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  cachedServer = await bootstrapServer();
+  return awsServerlessExpress.proxy(cachedServer, event, context, 'PROMISE').promise;
+};
+
