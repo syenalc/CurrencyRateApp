b09afd89f8b12176299b0b532726cd0a7e12056f
syenalc <shunpei.yopuedo@gmail.com>
2024-09-02 01:41:14 +0900

    apiエンドポイント変更

--- frontend/my-app/src/utils/api.ts
+++ frontend/my-app/src/utils/api.ts
@@ -1,8 +1,11 @@
 import { Item } from "./items.model";
+import { User } from "./users.model";
+
+const API_BASE_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:4000';
 
 export async function findAll(){
     try{
-        const res = await fetch('http://localhost:4000/items',{
+        const res = await fetch('${API_BASE_URL}/items',{
             method:"GET",
         });
         if (!res.ok) {
@@ -17,7 +20,7 @@ export async function findAll(){
 
 export async function create(item:Item){
     try{
-        const res = await fetch('http://localhost:4000/items',{
+        const res = await fetch('${API_BASE_URL}/items',{
             method:"POST",
             headers: {
                 'Content-Type': 'application/json',
@@ -28,6 +31,25 @@ export async function create(item:Item){
             throw new Error(`エラーが発生しました。ステータス:${res.status}`);
         }
         const data:Item[]= await res.json();
+        return data;
+    } catch (e){
+        console.log('エラーが発生しました', e);
+    }
+};
+
+export async function signUp(user:User){
+    try{
+        const res = await fetch('${API_BASE_URL}/signup',{
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user), // フロントエンドで入力されたデータを送信
+        });
+        if (!res.ok) {
+            throw new Error(`エラーが発生しました。ステータス:${res.status}`);
+        }
+        const data:User[]= await res.json();
         return data;
     } catch (e){
         console.log('エラーが発生しました', e);
