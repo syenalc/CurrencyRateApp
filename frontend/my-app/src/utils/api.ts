2130f6808e4c4152b5a2e71300d9d7a785e14049
syenalc <shunpei.yopuedo@gmail.com>
2024-09-04 07:16:59 +0900

    update, delete api 追加

--- frontend/my-app/src/utils/api.ts
+++ frontend/my-app/src/utils/api.ts
@@ -41,21 +41,60 @@ export async function create(item:Item){
     }
 };
 
-export async function signUp(user:User){
+export async function update(item:Item){
     try{
-        const res = await fetch(`${API_BASE_URL}/signup`,{
-            method:"POST",
+        const res = await fetch(`${API_BASE_URL}/items/${item.id}`,{
+            method:"PUT",
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(user), // フロントエンドで入力されたデータを送信
+            body: JSON.stringify(item), // フロントエンドで入力されたデータを送信
         });
         if (!res.ok) {
-            throw new Error(`エラーが発生しました。ステータス:${res.status}`);
+            const errorBody = await res.text(); // レスポンスボディをテキストで取得
+            throw new Error(`エラーが発生しました。ステータス: ${res.status}, メッセージ: ${errorBody}`);
         }
-        const data:User[]= await res.json();
-        return data;
+        const updatedItem: Item = await res.json();
+        return updatedItem;
     } catch (e){
         console.log('エラーが発生しました', e);
+        throw e;
     }
 };
+
+export async function deleteItem(itemId:string){
+    try{
+        const res = await fetch(`${API_BASE_URL}/items/${itemId}`,{
+            method:"DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!res.ok) {
+            const errorBody = await res.text(); // レスポンスボディをテキストで取得
+            throw new Error(`エラーが発生しました。ステータス: ${res.status}, メッセージ: ${errorBody}`);
+        }
+        return {};
+    } catch (e){
+        console.log('エラーが発生しました', e);
+        throw e;
+    }
+};
+// export async function signUp(user:User){
+//     try{
+//         const res = await fetch(`${API_BASE_URL}/signup`,{
+//             method:"POST",
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(user), // フロントエンドで入力されたデータを送信
+//         });
+//         if (!res.ok) {
+//             throw new Error(`エラーが発生しました。ステータス:${res.status}`);
+//         }
+//         const data:User[]= await res.json();
+//         return data;
+//     } catch (e){
+//         console.log('エラーが発生しました', e);
+//     }
+// };
