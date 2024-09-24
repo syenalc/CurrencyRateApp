86ee9a97270f346e3fa6669ede584042ea803f65
syenalc <shunpei.yopuedo@gmail.com>
2024-09-25 01:04:09 +0900

    サインアップ後bar編集

--- frontend/my-app/src/components/SignUpForm.tsx
+++ frontend/my-app/src/components/SignUpForm.tsx
@@ -6,8 +6,13 @@ import { Typography } from '@mui/material';
 import { signup } from '../utils/api';
 import { Auth } from '../utils/users.model';
 import { v4 as uuidv4 } from 'uuid'; 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
+
+interface State extends SnackbarOrigin {
+  open: boolean;
+}
 
 export default function ComposedTextField() {
   const [name, setName] = useState(''); // ユーザー名の状態
@@ -15,6 +20,13 @@ export default function ComposedTextField() {
   const [password, setPassword] = useState(''); // パスワードの状態
   const [error, setError] = useState<string | null>(null); // エラーメッセージの状態
   const navigate = useNavigate();
+
+  const [state, setState] = React.useState<State>({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center',
+  });
+  const { vertical, horizontal, open } = state;
 
   const handleSubmit = async() => {
     try {
@@ -34,12 +46,30 @@ export default function ComposedTextField() {
       setEmail('');
       setPassword('');
 
-      navigate('/');  // '/' へのリダイレクト
+      // スナックバーを開く
+      setState({ ...state, open: true });
+      
+      // 2秒待ってから '/' へのリダイレクトを実行
+      setTimeout(() => {
+        navigate('/');  // 2秒後にリダイレクト
+      }, 2500); // 2秒 (2000ms)
+
     } catch (e) {
       console.error('エラーが発生しました', e);
       setError('サインアップに失敗しました。もう一度お試しください。');
     }
   };
+  
+
+  // const handleClick = (newState: SnackbarOrigin) => () => {
+  //   setState({ ...newState, open: true });
+  // };
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
+
+  
   return (
     <>
     <Box>
@@ -97,9 +127,24 @@ export default function ComposedTextField() {
         onChange={(e) => setPassword(e.target.value)}
       />
     </Box>
-    <Button variant="contained" endIcon={<SendIcon />} onClick={handleSubmit}>
-        サインアップ
-    </Button>
+    <Box sx={{ width: 500 }}>
+      {/* ボタンをクリックするとサインアップ処理とスナックバーの表示を行う */}
+      <Button 
+            variant="contained" 
+            endIcon={<SendIcon />} 
+            onClick={handleSubmit}
+          >
+            サインアップ
+      </Button>
+      {/* スナックバー */}
+      <Snackbar
+          anchorOrigin={{ vertical, horizontal }}
+          open={open}
+          onClose={handleClose}
+          message="サインアップが完了しました。"
+          key={vertical + horizontal}
+      />
+    </Box>
     </Box>
   </>
   );
